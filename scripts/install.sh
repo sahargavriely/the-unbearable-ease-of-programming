767593865f45fd4e960124487dfecb52b6dc976c
sahargavriely <gav6sa@gmail.com>
2023-11-05 12:59:31 +0200

    7

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,6 +5,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
 
 function main {
+    pip install --upgrade pip
     pip install virtualenv
     python -m virtualenv .env --prompt "[brain-computer-interface] "
     find .env -name site-packages -exec bash -c 'echo "../../../../" > {}/self.pth' \;
