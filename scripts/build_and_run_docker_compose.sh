e310325f6a85a223f06680df45bd61ccfa019b30
sahargavriely <gav6sa@gmail.com>
2024-02-16 23:31:47 +0200

    script clearer

--- scripts/build_and_run_docker_compose.sh
+++ scripts/build_and_run_docker_compose.sh
@@ -1,4 +1,4 @@
-docker-compose up -d
-# docker exec -it brain-computer-interface sh
+docker-compose up -d --build
+# To enter shell in a container `docker exec -it <container-name> sh`
 # To stop this image you must use `docker-compose down` or `docker-compose down --rmi all` ro remove the image as well.
 # To rebuild this image you must use `docker-compose build` or `docker-compose up --build` to run the image as well.
