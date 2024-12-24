5b2e6118b63bcb0051200b64e61152f850b2b6d5
sahargavriely <gav6sa@gmail.com>
2024-12-24 23:47:10 +0000

    make sure timezone is the same with local machine across all areas

--- scripts/build_and_run_dockerfile.sh
+++ scripts/build_and_run_dockerfile.sh
@@ -2,3 +2,4 @@ docker build -f Dockerfile.base -t brain-computer-interface .
 
 # you might want to expose some ports using the -p flag
 docker run -ti --rm --name my-brain-computer-interface -v ./shared:/shared brain-computer-interface
+# you might want to connect time volumes with '-v /etc/localtime:/etc/localtime:ro -v /etc/timezone:/etc/timezone:ro -v /usr/share/zoneinfo:/usr/share/zoneinfo:ro'
