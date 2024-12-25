09eba6c612c22096edb41279e9d335aae0c89489
sahargavriely <gav6sa@gmail.com>
2024-12-25 00:22:35 +0000

    github stop supporting docker-compose and moed to docker compose

--- scripts/run_pipeline.sh
+++ scripts/run_pipeline.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 
-docker-compose --env-file docker.env up -d --build
+docker compose --env-file docker.env up -d --build
 # To enter shell in a container `docker exec -it <container-name> sh`
 # To rebuild this image you must use `docker-compose build` or `docker-compose up --build` to run the image as well.
--- scripts/stop_pipeline.sh
+++ scripts/stop_pipeline.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 
-docker-compose --env-file docker.env down
+docker compose --env-file docker.env down
 # Or `docker-compose --env-file docker.env down --rmi all` ro remove the image as well.
